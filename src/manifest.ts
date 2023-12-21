7e6614cc6c7679281bfddf5187110eb9d9732292
ZiHeng <shadow1746556951@gmail.com>
2023-12-21 21:09:11 +0800

    feat: Added alert icons

--- src/manifest.ts
+++ src/manifest.ts
@@ -10,10 +10,10 @@ export default defineManifest({
   version: packageData.version,
   manifest_version: 3,
   icons: {
-    16: 'img/logo-16.png',
-    32: 'img/logo-34.png',
-    48: 'img/logo-48.png',
-    128: 'img/logo-128.png',
+    16: 'img/logo-16-light.png',
+    32: 'img/logo-32-light.png',
+    48: 'img/logo-48-light.png',
+    128: 'img/logo-128-light.png',
   },
   action: {
     default_popup: 'popup.html',
@@ -37,7 +37,12 @@ export default defineManifest({
   // },
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      resources: [
+        'img/logo-16-light.png',
+        'img/logo-32-light.png',
+        'img/logo-48-light.png',
+        'img/logo-128-light.png',
+      ],
       matches: [],
     },
   ],
