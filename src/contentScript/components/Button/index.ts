6e280955680ed54c713fd486131f8fa87129a88d
ZiHeng <shadow1746556951@gmail.com>
2023-12-21 18:09:30 +0800

    feat: Added button size attribute

--- src/contentScript/components/Button/index.ts
+++ src/contentScript/components/Button/index.ts
@@ -1,9 +1,11 @@
-import { LitElement, html, css } from 'lit'
-import { customElement } from 'lit/decorators.js'
-import { TitleTree } from '../../interface'
+import { html, css } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
 import { WCWaves } from '../Waves'
+import { styleMap } from 'lit/directives/style-map.js'
 
-export interface WCButtonOptions {}
+export interface WCButtonOptions {
+  size: 'small' | 'normal' | 'large'
+}
 
 @customElement('wc-button')
 export class WCButton extends WCWaves {
@@ -11,24 +13,47 @@ export class WCButton extends WCWaves {
     ...WCWaves.styles,
     css`
       :host button {
-        padding: 0;
         border: none;
         cursor: pointer;
         background: none;
         display: flex;
         justify-content: center;
         align-items: center;
-        padding: 6px;
       }
     `,
   ]
 
+  @property({ type: String })
+  size: WCButtonOptions['size'] = 'normal'
+
   constructor(options: WCButtonOptions) {
     super()
-    console.log(this.children)
+    console.log(this.size)
+  }
+
+  /** 根据 size 属性获取对应的数组 */
+  getValueBySize() {
+    let size = 12
+    switch (this.size) {
+      case 'small':
+        size = 4
+        break
+      case 'normal':
+        size = 8
+        break
+      case 'large':
+        size = 12
+        break
+    }
+
+    return size
   }
 
   render() {
-    return html`<button class="wc-button waves-effect">${this.children}</button>`
+    const style = { padding: `${this.getValueBySize()}px` }
+
+    return html`<button class="wc-button waves-effect" style=${styleMap(style)}>
+      ${this.children}
+    </button>`
   }
 }
