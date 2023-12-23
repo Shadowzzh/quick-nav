39ea9009014b9377fffbdae50141e1a9038e5a00
ZiHeng <shadow1746556951@gmail.com>
2023-12-23 23:22:38 +0800

    feat: Added getOffsetByElement 获取元素的偏移量

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,15 @@ export const $: (query: string, target?: Element) => Element[] = (
   query: string,
   target = document.body.parentElement!,
 ) => Array.from(target.querySelectorAll(query))
+
+/** 获取元素的偏移量 - translate(x ,y) */
+export const getOffsetByElement = (el: HTMLElement) => {
+  const translate = el.style.transform
+  const match = translate.match(/translate\((.+)px,(.+)px\)/) ?? []
+  const originOffset = {
+    x: Number(match[1] ?? 0),
+    y: Number(match[2] ?? 0),
+  }
+
+  return originOffset
+}
