5a9fa8f5be9c3e7bcd07ff2ecff9f290cf1caa66
ziheng <shadow1746556951@gmail.com>
2024-01-09 14:44:57 +0800

    feat: 删除 popup页面

--- src/manifest.ts
+++ src/manifest.ts
@@ -16,7 +16,7 @@ export default defineManifest({
     128: 'img/logo-128-light.png',
   },
   action: {
-    default_popup: 'popup.html',
+    // default_popup: 'popup.html',
     default_icon: 'img/logo-48.png',
   },
   // options_page: 'options.html',
