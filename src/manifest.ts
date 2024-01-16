64a6111d8f4542b643c51610e1159a872d783636
ziheng <shadow1746556951@gmail.com>
2024-01-16 10:23:27 +0800

    chore: 增加不同尺寸的 logo

--- src/manifest.ts
+++ src/manifest.ts
@@ -13,18 +13,15 @@ export default defineManifest({
   version: packageData.version,
   manifest_version: 3,
   icons: {
-    16: 'img/logo-256.png',
-    32: 'img/logo-256.png',
-    48: 'img/logo-256.png',
-    128: 'img/logo-256.png',
+    16: 'img/logo-16.png',
+    32: 'img/logo-32.png',
+    48: 'img/logo-48.png',
+    128: 'img/logo-128.png',
+    256: 'img/logo-256.png',
   },
   action: {
-    // default_popup: 'popup.html',
-    default_icon: 'img/logo-256.png',
-    // default_title: isProduction ? undefined : '测',
+    default_icon: 'img/logo-128.png',
   },
-  // options_page: 'options.html',
-  // devtools_page: 'devtools.html',
   background: {
     service_worker: 'src/background/index.ts',
     type: 'module',
@@ -36,17 +33,17 @@ export default defineManifest({
       js: [isProduction ? 'src/contentScript/index.ts' : 'src/contentScript/launch.ts'],
     },
   ],
-  // side_panel: {
-  //   default_path: 'sidepanel.html',
-  // },
   web_accessible_resources: [
     {
-      resources: ['img/logo-256.png', 'img/logo-256.png', 'img/logo-256.png', 'img/logo-256.png'],
+      resources: [
+        'img/logo-16.png',
+        'img/logo-32.png',
+        'img/logo-48.png',
+        'img/logo-128.png',
+        'img/logo-256.png',
+      ],
       matches: [],
     },
   ],
-  permissions: ['sidePanel', 'storage', 'activeTab', 'scripting'],
-  // chrome_url_overrides: {
-  // newtab: 'newtab.html',
-  // },
+  permissions: ['storage', 'activeTab'],
 })
